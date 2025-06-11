874f0f48687cd591b4470263a13ded0a186c583f
Isak Larsson <isak.larsson@savr.com>
2025-06-11 15:27:23 +0200

    remove unused

--- lua/options.lua
+++ lua/options.lua
@@ -3,8 +3,6 @@ require "nvchad.options"
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "red" })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = "yellow" })
 
--- add yours here!
-
 local o = vim.o
 
 o.cursorlineopt = "both" -- to enable cursorline!
