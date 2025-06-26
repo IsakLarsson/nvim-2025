8bf219413a4c0f8b56c8435584d12d029b5ca40c
Isak Larsson <isak.larsson@savr.com>
2025-06-26 14:30:03 +0200

    borders on lsp hover window

--- init.lua
+++ init.lua
@@ -35,3 +35,13 @@ require "autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+--Rounded lsp hover borders
+local orig_util = vim.lsp.util.open_floating_preview
+
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.util.open_floating_preview = function(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or "rounded"
+  return orig_util(contents, syntax, opts, ...)
+end
