2922494e2b2b446c4a222038fae0f9a2b920c4ab
Isak Larsson <isak.larsson@savr.com>
2025-09-12 13:45:59 +0200

    add undotree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,6 +20,18 @@ return {
     end,
   },
   {
+    "jiaoshijie/undotree",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    ---@module 'undotree.collector'
+    ---@type UndoTreeCollector.Opts
+    opts = {
+      -- your options
+    },
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
