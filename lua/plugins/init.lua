c826b9d6ca41e10d989c8442dcd9f9b511713d4a
Isak Larsson <isaklarsson98@hotmail.com>
2025-10-30 10:35:17 +0100

    remove flash stuff i didnt like

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -156,38 +156,13 @@ return {
     opts = {
       modes = {
         search = { enabled = false },
-        char = { multi_line = false, highlight = { backdrop = false }, jump_labels = true },
+        char = { multi_line = false, highlight = { backdrop = false } },
       },
       highlight = {
         backdrop = true,
       },
     },
     keys = {
-      {
-        "<leader>fd",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump {
-            matcher = function(win)
-              ---@param diag Diagnostic
-              return vim.tbl_map(function(diag)
-                return {
-                  pos = { diag.lnum + 1, diag.col },
-                  end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
-                }
-              end, vim.diagnostic.get(vim.api.nvim_win_get_buf(win)))
-            end,
-            action = function(match, state)
-              vim.api.nvim_win_call(match.win, function()
-                vim.api.nvim_win_set_cursor(match.win, match.pos)
-                vim.diagnostic.open_float()
-              end)
-              state:restore()
-            end,
-          }
-        end,
-        desc = "Flash",
-      },
       {
         "<leader>s",
         mode = { "n", "x", "o" },
